5d66115301a4380a6a6f3013b727b96e77e61949
Andre Saliba <andrecstk@gmail.com>
2022-06-11 18:51:42 -0300

    Added fetch to front end

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-
+import { useEffect, useState } from 'react';
 import './App.css';
 
 export function App() {
+  const [API, setAPI] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/repos')
+      .then((res) => res.json())
+      .then((data) => setAPI(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Repos</h1>
+      <h2>{API}</h2>
     </div>
   );
 }
